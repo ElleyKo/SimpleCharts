2978b99301d7223361ea13edcb4d052b6de0cb9e
elleyko <yelena.korneeva@ya.ru>
2020-12-29 17:13:36 +0300

    dinamic area and line

--- src/components/charts/FilledLineWithGrid.vue
+++ src/components/charts/FilledLineWithGrid.vue
@@ -1,29 +1,37 @@
 <template>
-  <svg class="container" :viewBox="viewBox">
+  <svg class="container" v-if="isLoaded" :viewBox="viewBox">
     <g class="x-axis" :transform="transformXParams">
-      <line
-        v-for="item in chartData"
-        :key="item.x"
-        class="grid-line"
-        :x1="zeroValue"
-        :x2="zeroValue"
-        :y1="zeroValue"
-        :y2="y2ValueXAxis"
-      />
+      <g
+        class="tick"
+        v-for="point in $options.chartData"
+        :transform="'translate(' + scaleX(point.x) + ', 0)'"
+        :key="point.x"
+      >
+        <line y2="6" x2="0"></line>
+        <text dy=".71em" y="9" x="0" style="text-anchor: middle;">
+          {{ point.x }}
+        </text>
+        <line class="grid-line" x1="0" y1="0" x2="0" y2="-440"></line>
+      </g>
     </g>
     <g class="y-axis" :transform="transformYParams">
-      <line
-        tick=""
-        class="grid-line"
-        :x1="zeroValue"
-        :x2="xAxisLength"
-        :y1="zeroValue"
-        :y2="zeroValue"
-      />
+      <g
+        class="tick"
+        v-for="point in $options.chartData"
+        :transform="'translate(0, ' + scaleY(point.x) + ')'"
+        style="opacity: 1;"
+        :key="point.x"
+      >
+        <line x2="-6" y2="0"></line>
+        <text dy=".32em" x="-9" y="0" style="text-anchor: end;">
+          {{ point.x }}
+        </text>
+        <line class="grid-line" x1="0" y1="0" x2="440" y2="0"></line>
+      </g>
     </g>
     <g>
-      <path></path>
-      <path></path>
+      <path :d="getArea" class="area"></path>
+      <path :d="getLine" class="area-border"></path>
     </g>
   </svg>
 </template>
@@ -31,7 +39,9 @@
 <script>
 import chartData from "@/components/charts/ChartMock.json";
 import * as d3 from "d3-scale";
+import * as d3Shape from "d3-shape";
 export default {
+  chartData,
   name: "FilledLineWithGrid",
   components: {},
   props: {
@@ -71,117 +81,102 @@ export default {
     y2ValueXAxis() {
       return -(this.height - 2 * this.margin);
     },
+    getArea() {
+      const area = d3Shape
+        .area()
+        .x((d) => d.x)
+        .y0(this.height - this.margin)
+        .y1((d) => d.y)
+        .curve(d3Shape.curveCatmullRom.alpha(0.3));
+      return area(this.rawData);
+    },
+    getLine() {
+      const line = d3Shape
+        .line()
+        .x((d) => d.x)
+        .y((d) => d.y)
+        .curve(d3Shape.curveCatmullRom.alpha(0.3));
+      return line(this.rawData);
+    },
   },
   data() {
     return {
+      isLoaded: false,
       zeroValue: 0,
+      scaleX: [],
+      scaleY: [],
+      rawData: [],
     };
   },
   methods: {
-    addAxisY(chartData) {
-      // функция интерполяции значений на ось Y
-      const scaleY = d3.scale
-        .linear()
-        .domain([100, 0])
-        .range([0, this.yAxisLength]);
-      // создаем ось Y
-      const yAxis = d3.svg
-        .axis()
-        .scale(scaleY)
-        .orient("left");
+    setRawData() {
+      const chartData = this.$options.chartData;
+      const rawData = [];
+      chartData.forEach((point) => {
+        rawData.push({
+          x: this.scaleX(point.x) + this.margin,
+          y: this.scaleY(point.y) + this.margin,
+        });
+      });
+      this.rawData = rawData;
     },
-    addAxisX(chartData) {
-      // функция интерполяции значений на ось Х
-      const scaleX = d3.scale
-        .linear()
+    setScales() {
+      this.scaleX = d3
+        .scaleLinear()
         .domain([0, 100])
         .range([0, this.xAxisLength]);
 
-      // функция интерполяции значений на ось Y
-      const scaleY = d3.scale
-        .linear()
+      this.scaleY = d3
+        .scaleLinear()
         .domain([100, 0])
         .range([0, this.yAxisLength]);
-      for (let i = 0; i < chartData.length; i++)
-        chartData.push({
-          x: scaleX(chartData[i].x) + this.margin,
-          y: scaleY(chartData[i].y) + this.margin,
-        });
+      console.log(this.scaleX);
+      console.log(this.scaleY);
     },
-    drawChart() {
-      // создаем ось X
-      const xAxis = d3.svg
-        .axis()
-        .scale(this.scaleX)
-        .orient("bottom");
-
-      // создаем набор вертикальных линий для сетки
-      d3.selectAll("g.x-axis g.tick")
-        .append("line")
-        .classed("grid-line", true)
-        .attr("x1", 0)
-        .attr("y1", 0)
-        .attr("x2", 0)
-        .attr("y2", -(this.height - 2 * this.margin));
-
-      // рисуем горизонтальные линии
-      d3.selectAll("g.y-axis g.tick")
-        .append("line")
-        .classed("grid-line", true)
-        .attr("x1", 0)
-        .attr("y1", 0)
-        .attr("x2", this.thxAxisLength)
-        .attr("y2", 0);
-
-      // функция, создающая по массиву точек линии
-      const line = d3.svg
-        .line()
-        .x(function(d) {
-          return d.x;
-        })
-        .y(function(d) {
-          return d.y;
-        });
-      // функция, создающая область
-      const area = d3.svg
-        .area()
-        .x(function(d) {
-          return d.x;
-        })
-        .y0(this.height - this.margin)
-        .y1(function(d) {
-          return d.y;
-        });
-
-      const g = container.append("g");
-      g.append("path")
-        .attr("d", area(chartData))
-        .style("fill", "lightblue");
-      g.append("path")
-        .attr("d", line(chartData))
-        .style("stroke", "steelblue")
-        .style("stroke-width", 2);
+    getX(d) {
+      return d.x;
     },
+    getY(d) {
+      return d.y;
+    },
+  },
+  mounted() {
+    this.setScales();
+    this.setRawData();
+    this.isLoaded = true;
   },
 };
 </script>
 
 <style scoped>
+.tick {
+  opacity: 1;
+}
 .path,
 .line {
   fill: none;
   stroke: #333;
+  stroke-width: 1px;
+  stroke-dasharray: 2;
 }
 .grid-line {
   stroke: #000;
   stroke-opacity: 0.2;
 }
 text {
-  font: 10px Verdana;
+  font: 14px Verdana;
+  fill: #7892b6;
+}
+.area {
+  fill: lightblue;
+  opacity: 50%;
+}
+.area-border {
+  stroke: steelblue;
+  stroke-width: 2;
 }
 .container {
-  height: 500px;
-  width: 500px;
-  margin: 30px;
+  max-width: 510px;
+  max-height: 500px;
 }
 </style>
